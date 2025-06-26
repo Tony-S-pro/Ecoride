76f9978ee3b3afa89fdd95350a75e0f4153a0995
Tony-S-pro <tonys78@protonmail.com>
2025-06-26 20:25:30 +0200

    fixed button on carpools page

--- public/assets/ajax/get-carpools.js
+++ public/assets/ajax/get-carpools.js
@@ -5,13 +5,14 @@ function getTable(result)
     let x='';
 
     x=x+`<div><button class="btn btn-warning btn-sm mt-2" data-bs-toggle="modal" data-bs-target="#myModal" id="btn-AddNew">Ajouter</button></div>`;
-    x=x+`<table class="table table-success table-striped mt-2">`;
+    x=x+`<div class="table-responsive">
+        <table class="table table-responsive table-condensed table-striped mt-2">`;
 
     x=x+`<thead>
                 <tr>
                     <th>Départ</th>
-                    <th>--</th>
-                    <th>--</th>
+                    <th></th>
+                    <th></th>
                     <th>Arrivée</th>
                     <th>Prix</th>
                     <th></th>
@@ -23,19 +24,40 @@ function getTable(result)
     {
         //use backticks `` to avoid concatenations (</td> + <td>)
         x=x+`
-            <tr>
+            <tr class="accordion-toggle collapsed"
+                id="accordion${i}"
+                data-mdb-collapse-init
+                data-mdb-parent="#accordion${i}"
+                href="#collapse${i}"
+                aria-controls="collapse${i}">
             <td>${result[i]['departure_date']}</td>
             <td>${result[i]['departure_time']}</td>
             <td>${result[i]['departure_city']}</td>
             <td>${result[i]['arrival_city']}</td>
             <td>${result[i]['price']}</td>
-            <td><button type="button" class="btn btn-success btn-sm">Détails</button></td>
+            <td><button type="button" data-bs-toggle="collapse" data-bs-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}" class="btn btn-success btn-sm">Détails</button></td>
+            </tr>
+
+            <tr class="hide-table-padding">
+                <td colspan="6"=>
+                    <div id="collapse${i}" class="collapse">
+                        <div class="p-3">
+                            <div>adr. dép. : ${result[i]['departure_address']}</div>
+                            <div>adr. arr. : ${result[i]['arrival_address']}</div>
+                            <div>durée (est.) : ${result[i]['travel_time']}h</div>
+                            <div>4,5/5 *, ${result[i]['price']} crédits, 3 places</div>
+                            <div>description : ${result[i]['description']} je rajoute du texte pour voir comment ca afect tout ca rt tout et tou.</div>
+                            <div>animaux: O/N, fumeur O/N, autes......</div>
+                        </div>
+                    </div>
+                </td>
             </tr>
         `;
     }
 
     x=x+`</tbody>
-            </table>`;
+        </table>
+    </div>`;
     
     return x;
 }
